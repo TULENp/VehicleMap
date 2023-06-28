62bf7ed0510973a693a0f83c7ea8ff450dc31385
TULENp <zh_kononenko3@mail.ru>
2023-06-28 21:22:16 +0300

    Add google map

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { AppNavigation } from './src/navigation/AppNavigation';
+import MapView, { Marker } from 'react-native-maps';
 
 export default function App() {
+
     return (
         <AppNavigation />
     );
--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -2,14 +2,34 @@ import { View, Text, Button } from 'react-native'
 import React from 'react'
 import { NavigationProp, useNavigation } from '@react-navigation/native';
 import { TAppNavigation } from '../types';
+import MapView, { Marker } from 'react-native-maps';
 
 export function Main() {
     const { navigate } = useNavigation<NavigationProp<TAppNavigation>>();
+
+    const region = {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+    }
+
     return (
         <View>
             <Text>Main</Text>
-            <Button title='to Settings' onPress={() => navigate('Settings')} />
-            <Button title='to Vehicle' onPress={() => navigate('Vehicle')} />
+            {/* Map  */}
+                <MapView
+                    style={{ width: '100%', height: '80%' }}
+                    initialRegion={region}>
+                    <Marker
+                        title='hello'
+                        coordinate={{
+                            latitude: 37.78825,
+                            longitude: -122.4324
+                        }} />
+                </MapView>
+                <Button title='to Settings' onPress={() => navigate('Settings')} />
+                <Button title='to Vehicle' onPress={() => navigate('Vehicle')} />
         </View>
     )
 }
